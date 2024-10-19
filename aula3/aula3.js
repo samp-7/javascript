8609433e89b7fbef29b585a6c2b377213ccdabf3
samp-7 <samavitoria7@gmail.com>
2024-10-19 05:16:22 -0300

    Initial commit

---
+++ aula3/aula3.js
@@ -0,0 +1,17 @@
+const diadema = 300000
+const positivo = `A cidade ${diadema} é habitavel`
+const negativo = `A cidade é ${diadema} um cu`
+
+ if (diadema > 20000) {
+     console.log(positivo)
+} else {
+    console.log(negativo)
+}
+
+ if (cicerodantas > 20000) {
+     console.log(positivo)
+ } else {
+    console.log(negativo)
+}
+
+
---
+++ aula5/aula5.js
@@ -0,0 +1,2 @@
+// web e a dom (kkkkkkkkkkk)
+
